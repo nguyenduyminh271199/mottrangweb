8dd1e5696ff73aa19fd2a4f0e7fd7ef523c5ca1a
khangzxrr <vokhinbabezx4@gmail.com>
2021-09-17 20:49:01 +0700

    creat new web

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Chúc mừng sinh nhật 22 tuổi!!!',
+    introDesc: `Hôm nay là một ngày thật đặc biệt, 
+	không chỉ đối với m là còn là với cả thế giới 
+	– Ngày kỉ niệm sự ra đời của một đứa xinh đẹp mà hơi bố láo, xạo lzzz.`,
+    btnIntro: 'nhấn để next =]]]]]]]]]',
+    title: 'Thấy t đẹp trai k ?? ',
+    desc: 'Nhấn chọn để tiếp còn nữa !!!!! ',
+    btnYes: 'Đẹp trai vãi lzz :3',
+    btnNo: 'Đéo là đéo :)))',
+    question:'Trên thế giới hơn 7 tỉ người mà sao t lại kiếm m nch m biết mà <3',
+    btnReply: 'next',
+    reply: 'Chuẩn bị block t đúng hog <33333333',
+    mess: 't biết mà 🥰.',
+    messDesc: 'Nếu hog có ý định block t thì qua mess nè',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100012823785071' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
